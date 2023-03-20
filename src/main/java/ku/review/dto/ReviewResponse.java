5cefd25ca388419170e0844190cc8ad2db9fec40
Phakarat <Phakarat.kh@ku.th>
2023-03-20 23:49:07 +0700

    Creating an API service

---
+++ src/main/java/ku/review/dto/ReviewResponse.java
@@ -0,0 +1,11 @@
+package ku.review.dto;
+
+import lombok.Data;
+import java.time.Instant;
+
+@Data
+public class ReviewResponse {
+    private String username;
+    private String reviewText;
+    private Instant createdAt;
+}
